72b983bd570b4ddaa76049eec9fa59e28c5c260c
connormayfield <connormayfield@gmail.com>
2019-06-07 19:11:57 -0500

    more routing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
+import { Link } from "react-router-dom";
 
 
 function App() {
@@ -10,18 +11,18 @@ function App() {
     <Layout>
         <Header title="Title" scroll>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
--- portfolio/src/components/main.js
+++ portfolio/src/components/main.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
-
+import AboutMe from './aboutme';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
 
 const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/aboutme" component={AboutMe} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/resume" component={Resume} />
+
     </Switch>
 )
 
